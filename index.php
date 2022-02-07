18c4ec6d5c1e46cfb28af75c2d191c259e226e53
audemar <audemarsadish@gmail.com>
2022-02-07 14:50:44 +0100

    première partie tarminée

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="cvmobile.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MON CV NUMERIQUE POUR MOBILE</title>
+</head>
+
+<body style="background-color: #1e2336; font-family: serif;">
+    <header>
+        <nav>
+                    <!--MENU BURGER MOBILE-->
+            <i class="fas fa-search"></i>
+                    <!--MENU WEB-->
+        </nav>
+        <div class="container" style="margin: 1%; padding: 20px;">
+            <div class="row">
+                <div class="col" style="background-color:#915e4f; margin: 8%; padding: 20px;">
+                    <img src="photocv.png" class="img-fluid" width="100%" height="auto" alt="photo de sadish" />
+                </div>
+                <div class="col fixed-right text-light row align-items-center">
+                    <p>
+                        <span class="h1">SADISH AUDEMAR<br />
+                            40ans<br />
+                            Opticien
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <section class="col color">
+            <div class="container-fluid text-center">
+                <div class="row">
+                    <div class="col-sm-12 text-light">
+                        <p style="color:rgb(61, 212, 56);">
+                            <i class="fas fa-school"></i>
+                            Suivant actuellement une formation de
+                            Développeur Web, à l'école de La Manu
+                            à Versailles qui à débuté le 1er Octobre
+                            et qui prendra fin le 29 Mars 2022,
+                            je vise le titre professionnel
+                            «Développeur web et web mobile ».
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="col color">
+            <div class="container-fluid text-center">
+                <div class="row">
+                    <div class="col-sm-12 text-light">
+                        <p>
+                            <i class="far fa-grin" style="color:#ffffff"></i>
+                            Autonome et rigoureux, mes premières expériences
+                            m’ont permis de renforcer mes acquis et d’appréhender
+                            les différents outils et techniques dans le domaine de
+                            l’optique. En quête de nouveaux challenges, je souhaite
+                            rejoindre une profession à laquelle je pourrais apporter
+                            cette rigueur et mon goût pour les nouvelles technologies.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </header>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="stick"></div>
+        </div>
+    </div>
+    <section class="col color">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 text-light">
+                    <p class="h5">Expériences</p>
+                    <p>
+                        OPTOVISION Montigny-Le-Bretonneux 10/2015 - 10/2021<br />
+                        -Analyse et expertise pour les opticiens<br />
+                        -Responsable d'assistance technique des outils de prise de mesure,
+                        d'aide à la vente et des logiciels de commandes
+                    </p>
+                    <p>
+                        KRYS Maurepas 01/2017 - 09/2015<br />
+                        -Responsable de l'atelier de montage<br />
+                        -Gestion des commandes de montures<br />
+                        -Mise en place du télé-détourage verre
+                    </p>
+                    <p>
+                        ALAIN AFFLELOU Vélizy-Villacoublay 08/2003 - 05/2006<br />
+                        -Vente: Prise en charge du client, gestion des litiges clients,<br />
+                        utilisation des appareils de prise de mesure<br />
+                        -Montage: Réalisation des montages
+                    </p>
+                    <p>
+                        GRAND OPTICAL Le Chesnay 04/2001 - 07/2003<br />
+                        -Présentation des caractéristiques des produits et des détails<br />
+                        techniques aux clients<br />
+                        -Fabrication des verres (Surfaçage et Traitement antireflet)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid text-center">
+        <div class="row d-flex justify-content-center">
+            <div class="stick"></div>
+        </div>
+    </div>
+    <section class="col color">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 text-light">
+                    <p class="h5">Formations</p>
+                    <p>
+                        ESSILOR ACADEMY EUROPE Paris 12
+                        -Réalisation d'examen de vue
+                    </p>
+                    <p>
+                        OPTOVISION Allemagne, Langen<br />
+                        - Logiciel Optique "WinOnLine"<br />
+                        -Appareil d'examen de vue et d'aide à la vente "I'Synchro"<br />
+                        -Tour de prise de mesure "VET3 by PASKAL 3D"
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="stick"></div>
+        </div>
+        <div class="col text-center text-light">
+            <p class="h5 diplome">Diplômes</p><br />
+            <p class="capbep">CAP-BEP OPTIQUE-LUNETTERIE</p>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="stick2"></div>
+        </div>
+    </div>
+    <section>
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-5 text-light color">
+                    <p class="h5">Autres compétences</p>
+                    <p>
+                        Langue:<br />
+                        Anglais intermédiaire B1
+                    </p>
+                    <p>
+                        INFORMATIQUE:<br />
+                        Bureautique =<br/>
+                        Word, Excel, Logiciels<br />
+                        optiques<br/>
+                        Programmation =<br/>
+                        HTML, CSS, GIT/GitHub,<br/>
+                        BOOTSTRAP, JAVASCRIPT, PHP, WORDPRESS
+                    </p>
+                </div>
+                <div class="col-5 text-light color">
+                    <p class="h5">Centre d'intérêt</p>
+                    <p>
+                        Nouvelles technologies:<br />
+                        Curieux d'apprendre les nouveautés
+                    </p>
+                    <p>
+                        Cuisine:<br />
+                        Préparer de nouvelles recettes<br />
+                    </p>
+                    <p>
+                        Bricolage:<br />
+                        Moments de tranquilité
+                    </p>
+                    <p>
+                        Voyages:<br />
+                        Passionné par les différentes cultures
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col text-light color">
+                    <p class="h5">Autres compétences humaines</p>
+                    <p class="text-center">
+                        Discipline et Persévérance
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="col text-center text-light footerbox">
+            <p class="h5">Contact</p><br />
+            <div class="row justify-content-center">
+                <div class="col-3 d-none d-md-block d-lg-block d-xl-block d-xxl-block justify-content-center">
+                    <i class="fas fa-mobile"></i>
+                    <p>+33 6 35 41 80 62</p>
+                </div>
+                <div class="col-3 d-none d-md-block d-lg-block d-xl-block d-xxl-block justify-content-center">
+                    <i class="fas fa-envelope"></i>
+                    <p>audemarsadish@gmail.com</p>
+                </div>
+                <div class="col-3 d-none d-md-block d-lg-block d-xl-block d-xxl-block justify-content-center">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>Ablis</p>
+                </div>
+            </div>
+            <!--footer pour version mobile-->
+            <div class="row justify-content-center">
+                <div class="col-12 d-block d-md-none d-lg-none d-xl-none d-xxl-none justify-content-center">
+                    <i class="fas fa-mobile"></i>
+                    <p>+33 6 35 41 80 62</p>
+                </div>
+                <div class="col-12 d-block d-md-none d-lg-none d-xl-none d-xxl-none justify-content-center">
+                    <i class="fas fa-envelope"></i>
+                    <p>audemarsadish@gmail.com</p>
+                </div>
+                <div class="col-12 d-block d-md-none d-lg-none d-xl-none d-xxl-none justify-content-center">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>Ablis</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="container justify-content-between text-center">
+        <div class="row text-light linkpart">
+            <div class="col 5">
+                <i class="fab fa-linkedin linkedin"></i>
+            </div>
+            <div class="col 5">
+                <i class="fab fa-github github"></i>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
